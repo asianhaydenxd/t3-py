1881849915383d095386237b768853018c273fcb
Hayden <asianhaydenxd@gmail.com>
2021-08-12 20:15:58 -0400

    Add string representation to Board class

--- t3.py
+++ t3.py
@@ -8,3 +8,11 @@ class Cell(Enum):
 class Board:
     def __init__(self, board: list=[[Cell.N for _ in range(3)] for _ in range(3)]):
         self.board = board
+    
+    def __str__(self) -> str:
+        string = ""
+        for y in self.board:
+            for x in y:
+                string += f"{x.value} "
+            string += "\n"
+        return string
