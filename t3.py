8702b88d3bae103a1721642d1762007402cdb414
Hayden <asianhaydenxd@gmail.com>
2021-08-12 20:14:01 -0400

    Add Cell enum class

--- t3.py
+++ t3.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
+class Cell(Enum):
+    X = "X"
+    O = "O"
+    N = "•"
+
 class Board:
     def __init__(self, board: list=[[Cell.N for _ in range(3)] for _ in range(3)]):
         self.board = board
