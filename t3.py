5c25e4d19357d15cef4903a29d63780553c312b6
Hayden <asianhaydenxd@gmail.com>
2021-08-12 23:14:57 -0400

    Create Board.get_winner method

--- t3.py
+++ t3.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 class Cell(Enum):
     X = "X"
@@ -15,3 +16,15 @@ class Board:
     def place(self, player: Cell, position: tuple):
         position_x, position_y = position
         self.board[position_y][position_x] = player
+    
+    def get_winner(self) -> Cell:
+        for board in [self.board, np.transpose(self.board)]:
+            for row in board:
+                if len(set(row)) == 1:
+                    return row[0]
+
+        if len(set([self.board[i][i] for i in range(len(self.board))])) == 1:
+            return self.board[0][0]
+        if len(set([self.board[i][-1-i] for i in range(len(self.board))])) == 1:
+            return self.board[0][-1]
+        return Cell.N
